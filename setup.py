35a2f97feecb8c840ebb05f5b1221bb501f2efc3
Hassan Abdelhadi <has8385s@hs-coburg.de>
2026-02-02 01:24:07 +0100

    fix config and setup files

--- setup.py
+++ setup.py
@@ -11,12 +11,19 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'rclpy',
+        'nav_msgs',
+        'geometry_msgs',
+        'derived_object_msgs',
+        'ackermann_msgs'
+    ],
     zip_safe=True,
-    maintainer='karan',
-    maintainer_email='karan@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Hassan Abdelhadi',
+    maintainer_email='has8385s@hs-coburg.de',
+    description='Trajectory Planning Node for STERILINK Autonomous Mobile Robot',
+    license='Apache License 2.0',
     extras_require={
         'test': [
             'pytest',
